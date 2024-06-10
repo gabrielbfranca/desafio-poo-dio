a723b3f8488e69e5d26ae244b85b356bcbf2b037
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-06-10 14:43:27 -0300

    lombok Conteudo.java

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,6 +1,7 @@
 package br.com.dio.desafio.dominio;
+import lombok.Data;
 
-public abstract class Conteudo {
+public abstract @Data class Conteudo {
 
     protected static final double XP_PADRAO = 10d;
 
@@ -8,20 +9,4 @@ public abstract class Conteudo {
     private String descricao;
 
     public abstract double calcularXp();
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
